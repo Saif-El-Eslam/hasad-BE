f7cf88c3621401120ea2c1240894e218e6b8319d
saifeleslam <saif82820@gmail.com>
2024-08-21 18:24:24 +0300

    went back

--- src/server.js
+++ src/server.js
@@ -1,40 +1,21 @@
-// import { config } from "./config/index.js";
-// import logger from "./utils/logger.js";
-// import { connectDB } from "./config/db.js";
-// import app from "./app.js";
+import { config } from "./config/index.js";
+import logger from "./utils/logger.js";
+import { connectDB } from "./config/db.js";
+import app from "./app.js";
 
-// const startServer = async () => {
-//   try {
-//     // Connect to Database
-//     await connectDB();
-
-//     // Start Server
-//     app.listen(config.port, () => {
-//       logger.info(`Server running on port ${config.port}`);
-//     });
-//   } catch (error) {
-//     console.error("Failed to start server:", error);
-//     process.exit(1);
-//   }
-// };
-
-// startServer();
-
-import logger from "../utils/logger.js";
-import { connectDB } from "../config/db.js";
-import app from "../app.js";
-
-// Convert your Express server into a serverless function for Vercel
-const handler = async (req, res) => {
+const startServer = async () => {
   try {
+    // Connect to Database
     await connectDB();
 
-    // Handle the request with your Express app
-    return app(req, res);
+    // Start Server
+    app.listen(config.port, () => {
+      logger.info(`Server running on port ${config.port}`);
+    });
   } catch (error) {
-    logger.error("Failed to handle request:", error);
-    res.status(500).json({ message: "Internal Server Error" });
+    console.error("Failed to start server:", error);
+    process.exit(1);
   }
 };
 
-export default handler;
+startServer();
