3fca7cb9c408f6510f8b2e1a0bae315c31fddc85
saifeleslam <saif82820@gmail.com>
2024-08-21 18:45:06 +0300

    added component-config.yaml

--- src/server.js
+++ src/server.js
@@ -3,19 +3,34 @@ import logger from "./utils/logger.js";
 import { connectDB } from "./config/db.js";
 import app from "./app.js";
 
-const startServer = async () => {
+// const startServer = async () => {
+//   try {
+//     // Connect to Database
+//     await connectDB();
+
+//     // Start Server
+//     app.listen(config.port, () => {
+//       logger.info(`Server running on port ${config.port}`);
+//     });
+//   } catch (error) {
+//     console.error("Failed to start server:", error);
+//     process.exit(1);
+//   }
+// };
+
+// startServer();
+
+const handler = async (req, res) => {
   try {
     // Connect to Database
     await connectDB();
 
-    // Start Server
-    app.listen(config.port, () => {
-      logger.info(`Server running on port ${config.port}`);
-    });
+    // Use Express app to handle the request
+    return app(req, res);
   } catch (error) {
-    console.error("Failed to start server:", error);
-    process.exit(1);
+    logger.error("Failed to handle request:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
-startServer();
+export default handler;
