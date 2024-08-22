0da0bd236de4f12a802fcb3a2344f1d1508475de
saifeleslam <saif82820@gmail.com>
2024-08-22 14:20:33 +0300

    small adjustments in the logging midlleware and the config file

--- src/app.js
+++ src/app.js
@@ -3,21 +3,24 @@ import auth_routes from "./routes/auth_routes.js";
 import folders_routes from "./routes/folders_routes.js";
 import books_routes from "./routes/books_routes.js";
 // import benefits_routes from "./routes/benefits_routes.js";
+import { config } from "./config/index.js";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  console.log(
-    `Method: ${req.method} --`,
-    `URL: ${req.url} --`,
-    `Body: ${JSON.stringify(req.body)} --`
-  );
 
-  next();
-}); // Custom middleware
+config.environment === "development" &&
+  app.use((req, res, next) => {
+    console.log(
+      `Method: ${req.method} --`,
+      `URL: ${req.url} --`,
+      `Body: ${JSON.stringify(req.body)}`
+    );
+
+    next();
+  });
 
 // Add routes with /api prefix
 app.use("/api/auth", auth_routes);
--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
+  environment: process.env.ENVIRONMENT || "development",
   port: process.env.PORT || 3000,
-  dbUrl: process.env.DEV_DB_URL,
+  dbUrl:
+    process.env.ENVIRONMENT === "production"
+      ? process.env.PROD_DB_URL
+      : process.env.DEV_DB_URL,
   jwtSecret: process.env.JWT_SECRET,
 };
