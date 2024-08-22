ef07fc7648a3311364f90d51245bf10d970c5d6e
saifeleslam <saif82820@gmail.com>
2024-08-22 12:51:19 +0300

    added the benefits routes

--- src/routes/benefits_routes.js
+++ src/routes/benefits_routes.js
@@ -5,6 +5,33 @@ import benefitsValidation from "../validations/benefits_validation.js";
 
 const router = express.Router({ mergeParams: true });
 
+router.get(
+  "/favourites",
+  // (req, res, next) => {
+  //   if (req.params.bookId) {
+  //     return next("route");
+  //   }
+  //   next();
+  // },
+  authenticate,
+  benefitsValidation.favourites,
+  benefitsRoutes.favourites
+);
+
+router.put(
+  "/:id/favourite",
+  authenticate,
+  benefitsValidation.favourite,
+  benefitsRoutes.favourite
+);
+
+router.put(
+  "/:id/unfavourite",
+  authenticate,
+  benefitsValidation.unfavourite,
+  benefitsRoutes.unfavourite
+);
+
 router.get("/", authenticate, benefitsValidation.index, benefitsRoutes.index);
 router.post(
   "/",
@@ -23,33 +50,6 @@ router.delete(
   authenticate,
   benefitsValidation.destroy,
   benefitsRoutes.destroy
-);
-
-router.get(
-  "/favourites",
-  (req, res, next) => {
-    if (req.params.bookId) {
-      return next("route");
-    }
-    next();
-  },
-  authenticate,
-  benefitsValidation.favourites,
-  benefitsRoutes.favourites
-);
-
-router.put(
-  "/:id/favourite",
-  authenticate,
-  benefitsValidation.favourite,
-  benefitsRoutes.favourite
-);
-
-router.put(
-  "/:id/unfavourite",
-  authenticate,
-  benefitsValidation.unfavourite,
-  benefitsRoutes.unfavourite
 );
 
 export default router;
