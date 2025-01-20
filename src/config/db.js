d4e588bf0882f7421d2ac7c412b406ebb51e7716
saifeleslam <saif82820@gmail.com>
2025-01-20 17:18:24 +0200

    Don't reconnect to mongo if already connected

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,23 @@ import mongoose from "mongoose";
 import { config } from "./index.js";
 import logger from "../utils/logger.js";
 
+// Create a global variable to hold the database connection
+let isConnected = false;
+
 export const connectDB = async () => {
+  if (isConnected) {
+    // If already connected, do nothing
+    logger.info("MongoDB connection already established.");
+    return;
+  }
+
   try {
+    // Connect to MongoDB
     await mongoose.connect(config.dbUrl);
-    logger.info("MongoDB connected successfully");
+
+    // Mark the connection as established
+    isConnected = true;
+    logger.info("MongoDB connected successfully.");
   } catch (error) {
     logger.error("MongoDB connection error:", error);
     process.exit(1);
